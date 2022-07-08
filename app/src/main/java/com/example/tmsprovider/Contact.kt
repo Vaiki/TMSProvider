e844d47511b1161f08c539fb9105bf77b48daa28
gurchunov-1997@mail.ru <Flatronl1718s>
2022-07-08 14:33:31 +0300

    написал класс ContactsActivity.kt и Contact.kt

--- app/src/main/java/com/example/tmsprovider/Contact.kt
+++ app/src/main/java/com/example/tmsprovider/Contact.kt
@@ -1,5 +1,10 @@
 package com.example.tmsprovider
 
+import android.graphics.Bitmap
+
 class Contact {
 //    Todo("Создать поля контакта которые будем выводить в recyclerView")
+    var name = ""
+    var number = ""
+    var image: Bitmap? = null
 }
--- app/src/main/java/com/example/tmsprovider/ContactsActivity.kt
+++ app/src/main/java/com/example/tmsprovider/ContactsActivity.kt
@@ -1,7 +1,11 @@
 package com.example.tmsprovider
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.provider.MediaStore
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tmsprovider.databinding.ActivityContactsBinding
 
 class ContactsActivity : AppCompatActivity() {
@@ -12,6 +16,35 @@ class ContactsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityContactsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        contact_list.layoutManager = LinearLayoutManager(this)
 
+        btn_read_contacts.setOnClickListener {
+            val contactList: MutableList<Contact> = ArrayList()
+            val contacts = contentResolver.query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null,
+                null,
+                null,
+                null
+            )
+            while (contacts.moveToNext()) {
+                val name =
+                    contacts.getString(contacts.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+                val number =
+                    contacts.getString(contacts.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                val obj = Contact()
+                obj.name = name
+                obj.number = number
+                val photo_uri =
+                    contacts.getString(contacts.getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_URI))
+                if (photo_uri != null) {
+                    obj.image =
+                        MediaStore.Images.Media.getBitmap(contentResolver, Uri.parse(photo_uri))
+                }
+                contactList.add(obj)
+            }
+            contact_list.adapter = ContactsAdapter(contactList, this)
+            contacts.close()
+        }
     }
 }
